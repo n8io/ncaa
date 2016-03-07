8649e914485bc7fced2517b0f13fd72dfc337dce
Nate Clark <nathan.clark@ignitionone.com>
2016-03-07 09:40:28 -0500

    Renamed lint tasks to better match purpose

---
+++ build/gulp/tasks/lint-js-server.js
@@ -0,0 +1,22 @@
+const cwd = require(`cwd`);
+
+module.exports = (gulp, plugins, cfg) => {
+  gulp.task(`lint-js-server`, lintServer);
+
+  function lintServer() {
+    const customGulpFormatter = require(cwd(`build/gulp/customGulpFormatter`));
+
+    return gulp.src(cfg.js.server.src, {base: `./`})
+      .pipe(plugins.eslint({fix: true}))
+      .pipe(plugins.if(isFixed, gulp.dest(`.`)))
+      .pipe(plugins.eslint())
+      .pipe(plugins.eslint.format(customGulpFormatter))
+      .pipe(plugins.eslint.failAfterError())
+      ;
+  }
+
+  function isFixed(file) {
+    // Has ESLint fixed the file contents?
+    return file.eslint !== null && file.eslint.fixed;
+  }
+};
